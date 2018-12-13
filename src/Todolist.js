a5a10d1d60dc606295a554455837ef446a8072d8
leohlgctzby <18207221@qq.com>
2018-12-13 11:28:46 +0800

    'Todolistinit'

---
+++ src/Todolist.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Todolist extends Component {
+  render(){
+    return (
+      <div>
+        todolist
+      </div>
+    )
+  }
+}
+
+export default Todolist 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Todolist from './Todolist'
 
 
-ReactDOM.render(App, document.getElementById('root'));
+
+ReactDOM.render(<Todolist/>, document.getElementById('root'));
