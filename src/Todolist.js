752fa2cdbb7a437191b6e67816deb6f0432715b7
leohlgctzby <18207221@qq.com>
2018-12-17 21:32:21 +0800

    actionTypes

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,56 +1,64 @@
 import React, { Component } from "react";
 import "antd/dist/antd.css";
-import { Input, Button, List } from "antd";
-import store from './store'
+import { Button, Input, List } from "antd";
+import store from "./store";
+import { CHANGE_INPUT_VALUE, LIST_ADD_ITEM, LIST_ITEM_DELETE } from './store/actionTypes'
 
-class Todolist extends Component {
-
-  constructor(props){
+export default class Todolist extends Component {
+  constructor(props) {
     super(props);
     this.state = store.getState();
-    store.subscribe(this.handleStoreChange);
-    console.log(this.state)
+    store.subscribe(this.handleStoreChange)
+    console.log(this.state);
   }
   render() {
     return (
       <div style={{ marginTop: "10px", marginLeft: "10px" }}>
         <div>
           <Input
+            onChange={this.handleChange}
+            placeholder="Todolist"
             value={this.state.inputValue}
-            placeholder="todoinfo"
             style={{ width: "300px", marginRight: "10px" }}
-            onChange={this.handleInputChange}
           />
-          <Button type="primary" onClick={this.handleBtnClick}>提交</Button>
+          <Button type="primary" onClick={this.handleClick}>提交</Button>
         </div>
+
         <List
-          style={{ marginTop: "10px", width: "300px"}}
+          style={{ width: "300px", marginTop: "10px" }}
           bordered
           dataSource={this.state.list}
-          renderItem={item => <List.Item>{item}</List.Item>}
+          renderItem={(item, index) => <List.Item onClick={this.handleItemDelete.bind(this, index)} >{item}</List.Item>}
         />
       </div>
     );
   }
-  handleInputChange = (e) =>{
+
+  handleClick = () => {
     const action = {
-      type: 'change_input_value',
-      value: e.target.value
-    }
-    store.dispatch(action)
-    console.log(e.target.value)
+      type: LIST_ADD_ITEM,
+    };
+    store.dispatch(action);
   }
 
   handleStoreChange = () => {
-    this.setState(store.getState());
+    this.setState(store.getState())
   }
 
-  handleBtnClick = () => {
+  handleChange = (e) => {
     const action = {
-      type: 'add_todo_item'
-    }
-    store.dispatch(action)
+      type: CHANGE_INPUT_VALUE,
+      value: e.target.value
+    };
+    store.dispatch(action);
+    console.log(e.target.value);
+  }
+
+  handleItemDelete(index){
+    const action = {
+      type: LIST_ITEM_DELETE,
+      index: index
+    };
+    store.dispatch(action);
   }
 }
-
-export default Todolist;
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,20 +1,25 @@
 const defaultState = {
   inputValue: "",
   list: []
-};
+}
 
 export default (state = defaultState, action) => {
-  if (action.type === "change_input_value") {
+  if(action.type === "change_input_value"){
     const newState = JSON.parse(JSON.stringify(state));
     newState.inputValue = action.value;
     return newState;
   }
-  if(action.type === "add_todo_item"){
+  if(action.type === "list_add_item"){
     const newState = JSON.parse(JSON.stringify(state));
     newState.list.push(newState.inputValue);
     newState.inputValue = "";
     return newState;
   }
-  console.log(state, action);
+
+  if(action.type === "list_item_delete"){
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.list.splice(action.index, 1);
+    return newState;
+  }
   return state;
-};
+}
