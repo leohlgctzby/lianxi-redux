4c7b9484220780a05d93d18e077b89c2c4dfd93a
leohlgctzby <18207221@qq.com>
2018-12-17 10:51:45 +0800

    Todolist

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "antd/dist/antd.css";
+import { Input, Button, List } from "antd";
+
+const data = [
+  "Racing car sprays burning fuel into crowd.",
+  "Japanese princess to wed commoner.",
+  "Australian walks 100km after outback crash.",
+  "Man charged over missing wedding girl.",
+  "Los Angeles battles huge wildfires."
+];
 
 class Todolist extends Component {
-  render(){
+  render() {
     return (
-      <div>
-        todolist
+      <div style={{ marginTop: "10px", marginLeft: "10px" }}>
+        <div>
+          <Input
+            placeholder="todoinfo"
+            style={{ width: "300px", marginRight: "10px" }}
+          />
+          <Button type="primary">提交</Button>
+        </div>
+        <List
+          style={{ marginTop: "10px", width: "300px"}}
+          bordered
+          dataSource={data}
+          renderItem={item => <List.Item>{item}</List.Item>}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default Todolist 
+export default Todolist;
