53796453a9ef5cf7048c04370e6b1723f5f46a58
leohlgctzby <18207221@qq.com>
2019-02-20 10:37:09 +0800

    把所有action从todolist移到actionCreator中，加入redux-thunk

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
 import {
-  getChangeInputValue,
-  getHandleClick,
-  getHandleDelete
+  changeInputValue,
+  handleClick,
+  handleDelete
 } from "./store/actionCreators";
 
 const TodoList = props => {
@@ -17,7 +17,7 @@ const TodoList = props => {
   return (
     <div>
       <div>
-        <input type="text" value={inputValue} onChange={changeInputValue} />
+        <input type="text" value={inputValue} onChange={(e) => {changeInputValue(e)}} />
         <button onClick={handleClick}>提交</button>
       </div>
       <ul>
@@ -31,33 +31,13 @@ const TodoList = props => {
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    inputValue: state.inputValue,
-    list: state.list
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    changeInputValue(e) {
-      const action = getChangeInputValue(e.target.value);
-      dispatch(action);
-    },
-
-    handleClick() {
-      const action = getHandleClick();
-      dispatch(action);
-    },
-
-    handleDelete(index) {
-      const action = getHandleDelete(index);
-      dispatch(action);
-    }
-  };
-};
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+  state => ({
+    inputValue: state.inputValue,
+    list: state.list
+  }),
+  { changeInputValue,
+    handleClick,
+    handleDelete}
 )(TodoList);
--- src/store/actionCreators.js
+++ src/store/actionCreators.js
@@ -1,15 +1,36 @@
 import { ADD_ITEM, DELETE_ITEM, CHANGE_INPUT_VALUE } from "./actionTypes";
 
-export const getChangeInputValue = value => ({
+const getChangeInputValue = value => ({
   type: CHANGE_INPUT_VALUE,
   value
 });
 
-export const getHandleClick = () => ({
+const getHandleClick = () => ({
   type: ADD_ITEM
 });
 
-export const getHandleDelete = index => ({
+const getHandleDelete = index => ({
   type: DELETE_ITEM,
   index
 });
+
+export const changeInputValue = e => {
+  return dispatch => {
+    const action = getChangeInputValue(e.target.value);
+    dispatch(action);
+  };
+};
+
+export const handleClick = () => {
+  return dispatch => {
+    const action = getHandleClick();
+    dispatch(action);
+  };
+};
+
+export const handleDelete = index => {
+  return dispatch => {
+    const action = getHandleDelete(index);
+    dispatch(action);
+  };
+};
